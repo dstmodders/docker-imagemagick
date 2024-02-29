cc0012940d7052cd54688bc0c7bcbc08cc7320d0
Victor Popkov <victor@popkov.me>
2024-02-29 22:51:00 +0200

    Refactor generate-supported-tags.sh

--- generate-supported-tags.sh
+++ generate-supported-tags.sh
@@ -3,81 +3,70 @@
 BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
 COMMIT_ID=$(git rev-parse --verify HEAD)
 DISTS=('alpine' 'debian')
-VERSIONS=()
+JSON="$(cat ./versions.json)"
+REPOSITORY='https://github.com/dstmodders/docker-imagemagick'
+VERSIONS_KEYS=()
 
-mapfile -t VERSIONS < <(jq -r 'keys[]' ./versions.json)
-IFS=$'\n' VERSIONS=($(sort -rV <<< "${VERSIONS[*]}")); unset IFS
+mapfile -t VERSIONS_KEYS < <(jq -r 'keys[]' <<< "$JSON")
+# shellcheck disable=SC2207
+IFS=$'\n' VERSIONS_KEYS=($(sort -rV <<< "${VERSIONS_KEYS[*]}")); unset IFS
 
 readonly BASE_DIR
 readonly COMMIT_ID
 readonly DISTS
-readonly VERSIONS
-
-# https://stackoverflow.com/a/17841619
-function join_by {
-  local d="${1-}"
-  local f="${2-}"
-  if shift 2; then
-    printf %s "$f" "${@/#/$d}";
-  fi
-}
-
-function jq_value {
-  local from="$1"
-  local key="$2"
-  local name="$3"
-  jq -r ".[${key}] | .${name}" "${from}"
-}
+readonly JSON
+readonly REPOSITORY
+readonly VERSIONS_KEYS
 
 function print_url() {
   local tags="$1"
   local commit="$2"
-  local dist="$3"
-  local legacy="$4"
-
-  local url="[$tags](https://github.com/dstmodders/docker-imagemagick/blob/${commit}/latest/${dist}/Dockerfile)"
-  if [ "${legacy}" == 'true' ]; then
-    url="[$tags](https://github.com/dstmodders/docker-imagemagick/blob/${commit}/legacy/${dist}/Dockerfile)"
-  fi
-
-  echo "- ${url}"
+  local directory="$3"
+  local url="[$tags]($REPOSITORY/blob/$commit/$directory/Dockerfile)"
+  echo "- $url"
 }
 
-cd "${BASE_DIR}" || exit 1
+cd "$BASE_DIR" || exit 1
 
 printf "## Supported tags and respective \`Dockerfile\` links\n\n"
 
-for v in "${VERSIONS[@]}"; do
+# reference:
+#   7.1.1-29-alpine, 7.1.1-29, alpine, latest
+#   legacy-6.9.13-7-alpine, legacy-6.9.13-7, legacy-alpine, legacy-latest, legacy
+for key in "${VERSIONS_KEYS[@]}"; do
   for dist in "${DISTS[@]}"; do
-    commit="${COMMIT_ID}"
-    version=$(jq -r ".[${v}] | .version" ./versions.json)
-    latest=$(jq_value ./versions.json "${v}" 'latest')
-    legacy=$(jq_value ./versions.json "${v}" 'legacy')
+    version=$(jq -r ".[$key] | .version" <<< "$JSON")
+    latest=$(jq -r ".[$key].latest" <<< "$JSON")
+    legacy=$(jq -r ".[$key].legacy" <<< "$JSON")
 
-    tag_dist="${dist}"
-    tag_full="${version}-${dist}"
-    tag_version="${version}"
+    tag_dist="$dist"
+    tag_full="$version-$dist"
+    tag_version="$version"
 
-    if [ "${legacy}" == 'true' ]; then
-      tag_dist="legacy-${tag_dist}"
-      tag_full="legacy-${tag_full}"
-      tag_version="legacy-${tag_version}"
+    if [ "$legacy" == 'true' ]; then
+      tag_dist="legacy-$tag_dist"
+      tag_full="legacy-$tag_full"
+      tag_version="legacy-$tag_version"
     fi
 
     tags=''
-    if [ "${dist}" == 'alpine' ]; then
-      tags="\`${tag_full}\`, \`${tag_version}\`, \`${tag_dist}\`"
-      if [ "${latest}" == 'true' ]; then
-        if [ "${legacy}" == 'true' ]; then
-          tags="${tags}, \`legacy-latest\`, \`legacy\`"
+    if [ "$dist" == 'alpine' ]; then
+      tags="\`$tag_full\`, \`$tag_version\`, \`$tag_dist\`"
+      if [ "$latest" == 'true' ]; then
+        if [ "$legacy" == 'true' ]; then
+          tags="$tags, \`legacy-latest\`, \`legacy\`"
         else
-          tags="${tags}, \`latest\`"
+          tags="$tags, \`latest\`"
         fi
       fi
     else
-      tags="\`${tag_full}\`, \`${tag_dist}\`"
+      tags="\`$tag_full\`, \`$tag_dist\`"
     fi
 
-    print_url "${tags}" "${commit}" "${dist}" "${legacy}"
+    if [ "$legacy" == 'true' ]; then
+      print_url "$tags" "$COMMIT_ID" "legacy/$dist"
+    else
+      print_url "$tags" "$COMMIT_ID" "latest/$dist"
+    fi
   done
 done
