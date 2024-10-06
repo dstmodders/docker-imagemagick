b2ba738e476ae99656824688f1f4621e5f1eae3f
Victor Popkov <victor@popkov.me>
2024-10-06 21:40:10 +0300

    Bump ImageMagick from 7.1.1-38 to 7.1.1-39

--- bin/generate-supported-tags.sh
+++ bin/generate-supported-tags.sh
@@ -36,7 +36,7 @@ cd "$BASE_DIR" || exit 1
 
 printf "## Supported tags and respective \`Dockerfile\` links\n\n"
 
-# reference: 7.1.1-38-alpine, 7.1.1-38, alpine, latest
+# reference: 7.1.1-39-alpine, 7.1.1-39, alpine, latest
 for key in "${LATEST_VERSIONS_KEYS[@]}"; do
   for dist in "${DISTS[@]}"; do
     version="$(jq -r ".latest | .[$key] | .version" <<< "$JSON")"
