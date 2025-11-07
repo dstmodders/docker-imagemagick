ec2d1b2972d8bb2b1302fef065e39278db078d13
Victor Popkov <victor@popkov.me>
2025-11-07 20:02:32 +0200

    Bump ImageMagick from 7.1.2-1 to 7.1.2-2

--- bin/bump-supported-tags.sh
+++ bin/bump-supported-tags.sh
@@ -101,7 +101,7 @@ print_url() {
   echo "- ${url}"
 }
 
-# reference: 7.1.2-1-alpine, 7.1.2-1, alpine, latest
+# reference: 7.1.2-2-alpine, 7.1.2-2, alpine, latest
 print_latest_tags() {
   for key in "${LATEST_VERSIONS_KEYS[@]}"; do
     for dist in "${DISTS[@]}"; do
