e83a5e036154fa22b0eb54ebcc15f2401df0022e
Victor Popkov <victor@popkov.me>
2025-11-08 11:48:58 +0200

    Bump ImageMagick from 6.9.13-28 to 6.9.13-29

--- bin/bump-supported-tags.sh
+++ bin/bump-supported-tags.sh
@@ -127,7 +127,7 @@ print_latest_tags() {
   done
 }
 
-# reference: legacy-6.9.13-28-alpine, legacy-6.9.13-28, legacy-alpine, legacy-latest, legacy
+# reference: legacy-6.9.13-29-alpine, legacy-6.9.13-29, legacy-alpine, legacy-latest, legacy
 print_legacy_tags() {
   for key in "${LEGACY_VERSIONS_KEYS[@]}"; do
     for dist in "${DISTS[@]}"; do
